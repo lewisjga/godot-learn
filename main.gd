6eaa78d9debed9837d1b8e013cbddec9ee9c9d7f
lewisjga <lewisjga@gmail.com>
2026-05-06 17:00:49 -0400

    Removed testing controls, put in placeholder inputs

--- main.gd
+++ main.gd
@@ -1,18 +1,1 @@
 extends Node
-
-
-@onready var player = $Player
-
-
-func _ready() -> void:
-	player.tc_changed.connect(_on_tc_changed)
-	print(player.total_compensation)
-	print(player.player_name)
-
-
-func _on_tc_changed(new_tc):
-	print("My name is %s and my total compensation is %s" % [player.player_name, new_tc])
-	if new_tc <= 400:
-		print("%s is a LOSER" % [player.player_name])
-	else:
-		print("%s works at FAANG" % [player.player_name])
--- player.gd
+++ player.gd
@@ -5,52 +5,35 @@ extends CharacterBody3D
 
 @export_range(0.0, 1.0) var mouse_sensitivity = 0.01
 @export var tilt_limit = deg_to_rad(75)
-@export var total_compensation = 300
-@export var player_name = "Gary"
 @export var speed = 5.0
 @export var acceleration = 4.0
 @export var jump_speed = 8.0
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-var jumping = false
 
 
-signal tc_changed(new_tc)
-
-
-func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("ui_up"):
-		total_compensation += 50
-		tc_changed.emit(total_compensation)
-	if Input.is_action_just_pressed("ui_down"):
-		total_compensation -= 50
-		tc_changed.emit(total_compensation)
-
-
-func _physics_process(delta):
-	velocity.y += -gravity * delta
+func _physics_process(delta: float):
+	if not is_on_floor():
+		velocity.y -= gravity * delta
 	get_move_input(delta)
 	move_and_slide()
 
 
 func _unhandled_input(event: InputEvent) -> void:
-	# Mouselook implemented using `screen_relative` for resolution-independent sensitivity.
 	if event is InputEventMouseMotion:
 		_camera_pivot.rotation.x -= event.screen_relative.y * mouse_sensitivity
-		# Prevent the camera from rotating too far up or down.
 		_camera_pivot.rotation.x = clampf(_camera_pivot.rotation.x, -tilt_limit, tilt_limit)
 		_camera_pivot.rotation.y += -event.screen_relative.x * mouse_sensitivity
 
 
-func get_move_input(delta):
+func get_move_input(delta: float):
 	var vy = velocity.y
 	velocity.y = 0
 
-	var input = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var dir = Vector3(input.x, 0, input.y).rotated(Vector3.UP, _camera_pivot.rotation.y)
 	velocity = lerp(velocity, dir * speed, acceleration * delta)
 
-	if Input.is_action_just_pressed("ui_accept"):
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		vy = jump_speed
-		jumping = true
 	velocity.y = vy
